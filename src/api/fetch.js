16df6c40e32bf3be27da1b9b21e174bed3e7b1ca
Kirill-nikonorov <Kirill-nikonorov@mail.ru>
2018-06-29 00:51:40 +0300

    Autorization header whith token added

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -3,15 +3,15 @@ import axios from 'axios';
 export default config => {
     const axiosConfig = config || {};
 
+    const token = JSON.parse(localStorage.getItem("redux")).token;
+
     axiosConfig.headers = {
         'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
+        'X-Requested-With': 'XMLHttpRequest',
+        'Authorization': `Token ${token}`
     };
 
     axiosConfig.method = axiosConfig.method || 'GET';
 
-    return axios(axiosConfig).then(response => {
-        console.log('responce = ', response);
-        return response;
-    });
+    return axios(axiosConfig)
 };
